dcdbd00fce29b132bbb7fa1c4e9bef3528b380aa
Leith Essid <essidleith@gmail.com>
2022-11-06 15:56:01 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in mobile_views.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
---
+++ lib/mobile_views.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative "mobile_views/version"
+
+module MobileViews
+  class Error < StandardError; end
+
+  def mobile_device?
+    user_agent = request.headers['User-Agent']
+    client = DeviceDetector.new(user_agent)
+    mobile_devices = %w[smartphone tablet feature_phone phablet smart_display wearable]
+    mobile_devices.include?(client.device_type)
+  end
+
+  def set_mobile_view_path
+    mobile_view_exists = File.exist?("#{Rails.root}/app/views/#{controller_name}/#{action_name}_mobile.html.erb")
+    return unless mobile_view_exists
+
+    self.action_name = "#{action_name}_mobile"
+    self.class.prepend_view_path Rails.root.join("app", "views", controller_name, "#{action_name}_mobile")
+  end
+
+  base.before_action :set_mobile_view_path, if: :mobile_device?
+end
